c36a50560dd6c535c23d3f8ac5cec6087baea797
sashidharsiddamsetty <S528172@mail.nwmissouri.edu>
2016-10-11 19:32:50 -0500

    Add files via upload

---
+++ gbapp.js
@@ -0,0 +1,23 @@
+var path = require("path");
+var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+
+var app = express();  // make express app
+var server = require('http').createServer(app);
+app.get('/',function(req,res)
+{
+	res.send('Helloo world');
+});
+
+// set up the view engine
+// manage our entries
+app.set("view EJS",'EJS');
+// GETS
+// POSTS
+// 404
+
+// Listen for an application request on port 8081
+server.listen(8081, function () {
+  console.log('Guestbook app listening on http://127.0.0.1:8081/');
+});
